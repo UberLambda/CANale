554f2277e122b6ecbc1db44254ea6af0dfdd3fe6
Paolo Jovon <paolo.jovon@gmail.com>
2019-06-22 21:29:07 +0100

    Added missing header guard in canale.h

--- include/canale.h
+++ include/canale.h
@@ -5,6 +5,8 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#ifndef CANALE_H
+#define CANALE_H
 
 #ifndef __cplusplus
 extern "C"
@@ -79,3 +81,5 @@ int caFlashMem(FILE *fp, unsigned devId, unsigned elfSize, unsigned char elfData
 #ifndef __cplusplus
 }
 #endif
+
+#endif // CANALE_H
