cc13370c7a77c29830ac7087258d3a2b9485eb51
Paolo Jovon <paolo.jovon@gmail.com>
2019-06-26 21:41:41 +0100

    Added dependency to CANnuccia (+ minor fixes to comms.cc)
    CANnuccia is cloned via SSH for now (until the repo goes public).

--- src/comms.cc
+++ src/comms.cc
@@ -1,5 +1,16 @@
+// CANale/src/comms.cc - Implementation of CANale/src/comms.hh
+//
+// Copyright (c) 2019, Paolo Jovon <paolo.jovon@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #include "comms.hh"
 
+extern "C"
+{
+#   include "common/can_msgs.h"
+}
 #include "moc_comms.cpp"
 
 namespace ca
@@ -11,6 +22,11 @@ Comms::Comms(QObject *parent)
 }
 
 Comms::~Comms() = default;
+
+void Comms::canRX(uint32_t id, size_t dataLen, const uint8_t data[])
+{
+    // FIXME IMPLEMENT
+}
 
 void Comms::progStart(unsigned devId, DeviceStats *outDeviceStats)
 {
