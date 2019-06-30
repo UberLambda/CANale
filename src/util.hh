788d538dfc6f9ccab2b44af2847daf586ec1075e
Paolo Jovon <paolo.jovon@gmail.com>
2019-06-30 18:27:41 +0100

    Added a std::streambuf + std::istream that reads from a memory block
    ELFIO reads from a std::istream; it will be constructed from the
    QByteArray that CAinst::flashELF() uses.

--- src/util.hh
+++ src/util.hh
@@ -9,6 +9,8 @@
 #define UTIL_HH
 
 #include <cstdint>
+#include <istream>
+#include <streambuf>
 
 namespace ca
 {
@@ -72,6 +74,58 @@ inline void writeU32LE(uint8_t outBytes[], uint32_t u32)
     outBytes[2] = (u32 & 0x00FF0000u) >> 16;
     outBytes[3] = (u32 & 0xFF000000u) >> 24;
 }
+
+/// A `std::streambuf` that operates on a memory block.
+// See: https://stackoverflow.com/a/13059195, https://stackoverflow.com/a/46069245
+class MemStreambuf : public std::streambuf
+{
+public:
+    MemStreambuf(uint8_t *data, size_t size)
+    {
+        auto dataP = reinterpret_cast<char *>(data);
+        setg(dataP, dataP, dataP + size);
+    }
+
+    pos_type seekpos(pos_type sp, std::ios_base::openmode which) override
+    {
+        return seekoff(sp - pos_type(off_type(0)), std::ios_base::beg, which);
+    }
+
+    pos_type seekoff(off_type off, std::ios_base::seekdir dir,
+                     std::ios_base::openmode which = std::ios_base::in) override
+    {
+        (void)which;
+
+        switch(dir)
+        {
+        case std::ios_base::beg:
+            setg(eback(), eback() + off, egptr());
+            break;
+
+        case std::ios_base::cur:
+            gbump(static_cast<int>(off));
+            break;
+
+        default: // std::ios_base::end
+            setg(eback(), egptr() + off, egptr());
+            break;
+        }
+
+      return gptr() - eback();
+    }
+};
+
+/// A `std::istream` that reads from a memory block.
+// See: https://stackoverflow.com/a/13059195
+class MemIStream : virtual MemStreambuf, public std::istream
+{
+public:
+    MemIStream (const uint8_t *data, size_t size)
+        : MemStreambuf(const_cast<uint8_t *>(data), size),
+          std::istream(static_cast<MemStreambuf *>(this))
+    {
+    }
+};
 
 }
 
