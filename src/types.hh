78a0dc4b3f4d129714fc2dbecbb861899711eced
Paolo Jovon <paolo.jovon@gmail.com>
2019-07-07 13:34:18 +0100

    Added a ca::LogHandler C++/Qt wrapper over CAlogHandler
    It will replace the `CAinst::logged()` signal.

--- src/types.hh
+++ src/types.hh
@@ -129,6 +129,86 @@ public slots:
     }
 };
 
+/// A C++/Qt equivalent of a `CAlogHandler`.
+struct LogHandler : public QObject
+{
+    Q_OBJECT
+
+public:
+    /// The actual log handler.
+    std::function<void(CAlogLevel level, const char *message)> handler;
+
+
+    /// Constructs a no-op log handler.
+    LogHandler(QObject *parent=nullptr)
+        : QObject(parent), handler()
+    {
+    }
+
+    /// Constructs a log handler from the given functor.
+    LogHandler(const decltype(handler) &handler, QObject *parent=nullptr)
+        : QObject(parent), handler(handler)
+    {
+    }
+    LogHandler(decltype(handler) &&handler, QObject *parent=nullptr)
+        : QObject(parent), handler(std::move(handler))
+    {
+    }
+
+    LogHandler(const LogHandler &toCopy)
+    {
+        operator=(toCopy);
+    }
+    LogHandler &operator=(const LogHandler &toCopy)
+    {
+        setParent(toCopy.parent());
+        handler = toCopy.handler;
+        return *this;
+    }
+
+    LogHandler(LogHandler &&toMove)
+    {
+        operator=(std::move(toMove));
+    }
+    LogHandler &operator=(LogHandler &&toMove)
+    {
+        setParent(toMove.parent());
+        handler = std::move(toMove.handler);
+        return *this;
+    }
+
+    virtual ~LogHandler() = default;
+
+    /// Returns whether the handler is valid or not.
+    inline operator bool() const
+    {
+        return bool(handler);
+    }
+
+
+    /// Invokes the log handler.
+    /// Emits `logged()` as appropriate.
+    inline void operator()(CAlogLevel level, QString message)
+    {
+        call(level, message);
+    }
+
+signals:
+    /// Emitted when a message is logged.
+    void logged(CAlogLevel level, QString message);
+
+public slots:
+    /// Equivalent to `operator()()`.
+    void call(CAlogLevel level, QString message)
+    {
+        if(handler)
+        {
+            handler(level, qPrintable(message));
+        }
+        emit logged(level, message);
+    }
+};
+
 }
 
 #endif // TYPES_HH
